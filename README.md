4d4dcaa0e09c83f98e6892e17c21493502202d7f
Jonathan Lin <jlin703@berkeley.edu>
2019-02-24 15:55:01 -0800

    template for tensorflow mini-assignment

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# dreamrl
+# DreamRL
+Building compact, generative models of environments for efficient reinforcement learning.
+
+## Contributors
+Jonathan Lin (project lead), Jihan Yin, Joey Hejna, Chelsea Chen, Andrew Chen, Michael Huang, Sumer Kohli, Anish Nag
---
+++ education/sin_tempate.py
@@ -0,0 +1,44 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+
+class Net:
+	def __init__(self):
+		# Your training data
+		self.x_train = np.linspace(-np.pi, np.pi * 2, num=10000)
+		self.y_train = np.sin(self.x_train) + self.x_train / 3 + (np.random.rand(10000) - 0.5) / 5
+
+		# Some parameters (feel free to edit these)
+		self.input_size, self.output_size = 1, 1
+		self.hidden_size1, self.hidden_size2 = 128, 128
+		self.num_epochs = 10000
+
+		# Starting a TensorFlow session
+		self.sess = tf.Session()
+		self._build_model()
+
+	def _build_model(self):
+		# TODO: Build your fully-connected neural net!
+		pass
+		
+	def train(self):
+		# TODO: Train your model. No need to do any batching.
+		for _ in range(self.num_epochs):
+			pass
+
+	def evaluate(self, x):
+		# TODO: Output your model's prediction for the input x
+		return 0
+
+# Plots the function your model represents (should look like the function f(x) = sin(x) + x / 3)
+def show_plot(model):
+	x = np.linspace(-2 * np.pi, 2 * np.pi, num=1000)
+	y = np.array([model.evaluate([[i]]) for i in x])
+	y = y[:,0,0]
+	plt.plot(x, y)
+	plt.show()
+
+sin_pred = Net()
+sin_pred.train()
+show_plot(sin_pred)
+
