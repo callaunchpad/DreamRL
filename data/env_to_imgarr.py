c0af4a584c4d696c8d8a848a13d0188b4b67e60b
Chelsea Chen <chelseacc@berkeley.edu>
2019-03-09 18:02:07 -0800

    given env, return image array

---
+++ data/env_to_imgarr.py
@@ -0,0 +1,38 @@
+import gym
+import numpy as np
+from numpy import load
+import argparse
+
+parser = argparse.ArgumentParser(description='Generate nested array of images given an environment.')
+
+parser.add_argument('env_name', type=str, help='environment name')
+parser.add_argument('--episodes', type=int, default=100, help='number of episodes')
+parser.add_argument('--steps', type=int, default=250, help='number of steps')
+
+
+# parser.add_argument('--box2d', type=bool, help='whether environment is box2d or not')
+
+if __name__ == "__main__":
+	args = parser.parse_args()
+	env = gym.make(args.env_name)
+	ep_arr = []
+	for i_episode in range(args.episodes): #100
+		observation = env.reset()
+		step_arr = []
+		for t in range(args.steps): #250
+			a = env.render(mode='rgb_array')
+			a = a[::5, ::5, :]
+			action = env.action_space.sample()
+			observation, reward, done, info = env.step(action)
+			# if done:
+			# 	break
+			step_arr.append(a)
+	            
+		ep_arr.append(step_arr)
+
+	# np.savez(args.env_name+args.episodes+args.steps, *ep_arr)
+	np.savez(args.env_name+"_"+str(args.episodes)+"_"+str(args.steps), *ep_arr)
+
+
+
+		
