de593931de8527e292e6bcf45fe5d02606654901
Joey Hejna <jhejna@berkeley.edu>
2019-03-16 17:35:58 -0700

    network, but the loss doesnt work

--- mdn-rnn/mdn.py
+++ mdn-rnn/mdn.py
@@ -4,12 +4,13 @@
 import numpy as np
 import keras
 from keras import backend as K
-from keras.layers import Layer, Dense
+from keras import layers
+from keras.models import Model, Sequential
 
 ## CONSTANTS
 logSqrtTwoPI = np.log(np.sqrt(2.0 * np.pi))
 
-class MDN(Layer):
+class MDN(layers.Layer):
 
 	def __init__(self, output_dim, **kwargs):
 		self.output_dim = output_dim
@@ -22,7 +23,7 @@ class MDN(Layer):
 									initializer='truncated_normal',
 									trainable=True)
 		self.bias = self.add_weight(name='kernel', 
-									shape=(input_shape[1], self.output_dim),
+									shape=(self.output_dim,),
 									initializer='zeros',
 									trainable=True)
 
@@ -30,7 +31,7 @@ class MDN(Layer):
 
 	def call(self, x):
 		output = K.dot(x, self.kernel)
-		output = K.add_bias(output, self.bias)
+		output = K.bias_add(output, self.bias)
 		return output
 
 	def compute_output_shape(self, input_shape):
@@ -43,37 +44,87 @@ def get_mdn_coef(output):
 	  return logmix, mean, logstd
 '''
 
-def get_mdn_coef(output):
-	# first column is the batch dimension
-	assert output.shape[0] % 3 == 0
+class MDNRNN():
+	def __init__(self, hyperparameters):
+		self.hps = hyperparameters
+		self.build_model()
 
-	num_components = output.shape[1] / 3
-	
-	logmix = output[:, :num_components]
-	mean = output[:,num_components: 2*num_components]
-	logstd = output[:, 2*num_components:]
+	def build_model(self):
+		# Batch dimension????
+		self.input = layers.Input(shape=(self.hps['max_seq_len'], self.hps['in_width']), dtype='float32')
+		# hidden_state, self.last_state
+		rnn_out, self.hidden_state, self.cell_state = layers.LSTM(self.hps['rnn_size'], return_sequences=True, return_state=True, dropout=0.0, recurrent_dropout=0.0)(self.input)
+		self.output = layers.TimeDistributed(MDN(self.hps['out_width'] * self.hps['kmix'] * 3))(rnn_out)
+		self.model = Model(self.input, self.output)
+		print("Input:", self.model.input)
+		print("Output:", self.model.output)
+		self.model.compile(optimizer='adam', loss=MDNRNN.mdn_loss())
+		
+	def train(self, x, y):
 
-	logmix = logmix - K.logsumexp(logmix, axis=1, keepdims=True)
+		self.model.fit(x, y, batch_size=self.hps['batch_size'], validation_split=self.hps['validation_split'])
 
-	return logmix, mean, logstd
+	def test(self, x, y):
+		loss, acc = self.model.evaluate(x, y, batch_size=self.hps[batch_size])
 
-def lognormal(y, mean, logstd):
-	return -0.5 * ((y - mean) / K.exp(logstd)) ** 2 - logstd - logSqrtTwoPI
+	def get_mdn_coef(output):
+		# first column is the batch dimension
+		assert output.shape[2] % 3 == 0
+		print(' in get mdn coef')
+		num_components = int(int(output.shape[2]) / 3)
+		
+		logmix = output[:, :, :num_components]
+		mean = output[:, :, num_components: 2*num_components]
+		logstd = output[:, :, 2*num_components:]
 
-def get_lossfunc(logmix, mean, logstd, y):
-	v = logmix + lognormal(y, mean, logstd)
-	v = K.logsumexp(v, 1, keepdims=True)
-	return -K.mean(v) #perhaps axis=1 and keepdims?
+		logmix = logmix - K.logsumexp(logmix, axis=2, keepdims=True)
 
-def mdn_loss():
-	def loss(y, output):
-		logmix, mean, logstd = get_mdn_coef(output)
-		return get_lossfunc(logmix, mean, logstd, y)
-	return loss
+		return logmix, mean, logstd
+
+	def lognormal(y, mean, logstd):
+
+		return -0.5 * ((y - mean) / K.exp(logstd)) ** 2 - logstd - logSqrtTwoPI
+
+	def get_lossfunc(logmix, mean, logstd, y):
+		print(' in get loss')
+
+		v = logmix + MDNRNN.lognormal(y, mean, logstd)
+		print('log normal shape', v.shape)
+		v = K.logsumexp(v, 2, keepdims=True)
+		print('log normal shape', v.shape)
+
+		out = -K.mean(v)
+		return out #perhaps axis=1 and keepdims?
+
+	def mdn_loss():
+		def loss(y, output):
+			print("OUTPUT SHAPE", output.shape)
+			logmix, mean, logstd = MDNRNN.get_mdn_coef(output)
+			print('IN LOSS - mix, mean, std:', logmix.shape, mean.shape, logstd.shape)
+			return MDNRNN.get_lossfunc(logmix, mean, logstd, y)
+		return loss
 
 def main():
 	# TODO: Write MDN test here
-	return
+	hps = {}
+	hps['batch_size'] = 7
+	hps['max_seq_len'] = 16
+	hps['in_width'] = 8 # latent + action
+	hps['out_width'] = 6 # Latent
+	hps['rnn_size'] = 10
+	hps['kmix'] = 3
+	hps['validation_split'] = 0.1
+
+	mdnrnn = MDNRNN(hps)
+	print("####################################")
+	print("FINISHED BUILD")
+	# X Size = (DIMS, max seq, in_width)
+	X = np.random.normal(size=(1000, hps['max_seq_len'], hps['in_width']))
+	
+	Y = np.random.normal(size=(1000, hps['max_seq_len'], hps['out_width']))
+
+	mdnrnn.train(X, Y)
+
 
 if __name__ == "__main__":
 	main()
