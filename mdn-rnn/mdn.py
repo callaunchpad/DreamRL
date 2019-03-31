d74525dee16a0e97d82cb637c6942de1b2e12a34
Joey Hejna <jhejna@berkeley.edu>
2019-03-31 01:53:46 -0700

    Functional MDN RNN

--- mdn-rnn/mdn.py
+++ mdn-rnn/mdn.py
@@ -1,17 +1,12 @@
 # Author: Joey Hejna, Jihan
-# Resource: https://github.com/yanji84/keras-mdn/blob/master/mdn.py
+# Resources: https://github.com/yanji84/keras-mdn/blob/master/mdn.py
+#            https://github.com/hardmaru/WorldModelsExperiments/blob/master/carracing/rnn/rnn.py
 
 import numpy as np
 import keras
 from keras import backend as K
-<<<<<<< HEAD
 from keras import layers
-from keras.models import Model, Sequential
-=======
-from keras.layers import Layer, Dense, Dropout, Activation, LSTM
-from keras.activations import softmax, tanh
-from keras.models import Sequential
->>>>>>> 1788777564a337a6cb290e589425416d24c9a8ad
+from keras.models import Model
 
 ## CONSTANTS
 logSqrtTwoPI = np.log(np.sqrt(2.0 * np.pi))
@@ -54,17 +49,32 @@ class MDNRNN():
 		self.hps = hyperparameters
 		self.build_model()
 
+
 	def build_model(self):
-		# Batch dimension????
+		# Create Input layer: out shape = (batch, maxlen, latent_size + actions)
 		self.input = layers.Input(shape=(self.hps['max_seq_len'], self.hps['in_width']), dtype='float32')
-		# hidden_state, self.last_state
-		rnn_out, self.hidden_state, self.cell_state = layers.LSTM(self.hps['rnn_size'], return_sequences=True, return_state=True, dropout=0.0, recurrent_dropout=0.0)(self.input)
-		self.output = layers.TimeDistributed(MDN(self.hps['out_width'] * self.hps['kmix'] * 3))(rnn_out)
+		# Create RNN Cell (with droput!). out shape = (batch, maxlen, RNN size)
+		rnn_out, self.hidden_state, self.cell_state = layers.LSTM(self.hps['rnn_size'], return_sequences=True, 
+													return_state=True, dropout=self.hps['dropout'], 
+													recurrent_dropout=self.hps['recurrent_dropout'])(self.input)
+		# Reshape RNN out to distribute sequences over batch dim. out shape = (batch*maxlen, RNN size)
+		rnn_out = self.output = layers.Lambda(
+								lambda x: K.reshape(x, (-1, self.hps['rnn_size'])), 
+								output_shape=(self.hps['rnn_size'],))(rnn_out)
+		# Apply MDN to each vector of every sequence seperatly. 
+		# Each RNN vector corresponds to out params, each with k gaussians defined by 3 params
+		# out shape = (batch * maxlen, out*kmix*3)
+		self.output = MDN(self.hps['out_width'] * self.hps['kmix'] * 3)(rnn_out)
+		# Again push everything to the batch dimension
+		# out shape = (batch * maxlen * out, kmix*3)
+		self.output = layers.Lambda(
+								lambda x: K.reshape(x, (-1, self.hps['kmix'] * 3)), 
+								output_shape=(self.hps['kmix'] * 3,))(self.output)
 		self.model = Model(self.input, self.output)
 		print("Input:", self.model.input)
 		print("Output:", self.model.output)
 		self.model.compile(optimizer='adam', loss=MDNRNN.mdn_loss())
-		
+	
 	def train(self, x, y):
 
 		self.model.fit(x, y, batch_size=self.hps['batch_size'], validation_split=self.hps['validation_split'])
@@ -74,56 +84,49 @@ class MDNRNN():
 
 	def get_mdn_coef(output):
 		# first column is the batch dimension
-		assert output.shape[2] % 3 == 0
-		print(' in get mdn coef')
-		num_components = int(int(output.shape[2]) / 3)
+		assert output.shape[1] % 3 == 0
+		num_components = int(int(output.shape[1]) / 3)
 		
-		logmix = output[:, :, :num_components]
-		mean = output[:, :, num_components: 2*num_components]
-		logstd = output[:, :, 2*num_components:]
+		logmix = output[:, :num_components]
+		mean = output[:, num_components: 2*num_components]
+		logstd = output[:, 2*num_components:]
 
-		logmix = logmix - K.logsumexp(logmix, axis=2, keepdims=True)
+		logmix = logmix - K.logsumexp(logmix, axis=1, keepdims=True)
 
 		return logmix, mean, logstd
 
 	def lognormal(y, mean, logstd):
-
 		return -0.5 * ((y - mean) / K.exp(logstd)) ** 2 - logstd - logSqrtTwoPI
 
 	def get_lossfunc(logmix, mean, logstd, y):
-		print(' in get loss')
-
 		v = logmix + MDNRNN.lognormal(y, mean, logstd)
-		print('log normal shape', v.shape)
-		v = K.logsumexp(v, 2, keepdims=True)
-		print('log normal shape', v.shape)
-
+		v = K.logsumexp(v, 1, keepdims=True)
 		out = -K.mean(v)
-		return out #perhaps axis=1 and keepdims?
+		return out
 
 	def mdn_loss():
 		def loss(y, output):
-			print("OUTPUT SHAPE", output.shape)
+			# reshape the y vector from (batch, maxlen, out) to (batch * maxlen * out, 1)
+			y = K.reshape(y, (-1, 1))
 			logmix, mean, logstd = MDNRNN.get_mdn_coef(output)
-			print('IN LOSS - mix, mean, std:', logmix.shape, mean.shape, logstd.shape)
 			return MDNRNN.get_lossfunc(logmix, mean, logstd, y)
 		return loss
 
 def main():
 	# TODO: Write MDN test here
 	hps = {}
-	hps['batch_size'] = 7
-	hps['max_seq_len'] = 16
-	hps['in_width'] = 8 # latent + action
-	hps['out_width'] = 6 # Latent
-	hps['rnn_size'] = 10
-	hps['kmix'] = 3
+	hps['batch_size'] = 100
+	hps['max_seq_len'] = 1000
+	hps['in_width'] = 35 # latent + action
+	hps['out_width'] = 32 # Latent
+	hps['rnn_size'] = 256
+	hps['kmix'] = 5
+	hps['dropout'] = 0.9
+	hps['recurrent_dropout'] = 0.9
 	hps['validation_split'] = 0.1
 
 	mdnrnn = MDNRNN(hps)
-	print("####################################")
 	print("FINISHED BUILD")
-	# X Size = (DIMS, max seq, in_width)
 	X = np.random.normal(size=(1000, hps['max_seq_len'], hps['in_width']))
 	
 	Y = np.random.normal(size=(1000, hps['max_seq_len'], hps['out_width']))
