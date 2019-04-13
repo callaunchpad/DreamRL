b1b3d8d56359ba2a89c4d80d0867647599c41e94
Jonathan Lin <jlin703@berkeley.edu>
2019-04-13 16:39:15 -0700

    train script with transitions but no training

--- data/extract_img_action.py
+++ data/extract_img_action.py
@@ -15,21 +15,20 @@ parser.add_argument('--size', type=int, default=120, help='width/height of image
 def compress_image(img, size=60):
 	return resize(img, (size, size), mode='reflect', anti_aliasing=True)
 
-if __name__ == "__main__":
-	args = parser.parse_args()
-	env = gym.make(args.env_name)
+def extract(env_name, episodes, steps, box2d, size):
+	env = gym.make(env_name)
 	ep_img_arr = []
 	ep_act_arr = []
-	for i_episode in range(args.episodes):
+	for i_episode in range(episodes):
 		observation = env.reset()
 		step_img_arr = []
 		step_act_arr = []
-		for t in range(args.steps):
-			if args.box2d:
+		for t in range(steps):
+			if box2d:
 				image = observation
 			else:
 				image = env.render(mode='rgb_array')
-			image = compress_image(image, size=args.size)
+			image = compress_image(image, size=size)
 			action = env.action_space.sample()
 			observation, reward, done, info = env.step(action)
 			if done:
@@ -39,9 +38,15 @@ if __name__ == "__main__":
 	            
 		ep_img_arr.append(step_img_arr)
 		ep_act_arr.append(step_act_arr)
+	img_path_name = env_name+"_img_"+str(episodes)+"_"+str(steps)
+	act_path_name = env_name+"_act_"+str(episodes)+"_"+str(steps)
+	np.savez_compressed(img_path_name, *ep_img_arr)
+	np.savez_compressed(act_path_name, *ep_act_arr)
+	return img_path_name, act_path_name
 
-	np.savez_compressed(args.env_name+"_img_"+str(args.episodes)+"_"+str(args.steps), *ep_img_arr)
-	np.savez_compressed(args.env_name+"_act_"+str(args.episodes)+"_"+str(args.steps), *ep_act_arr)
+if __name__ == "__main__":
+	args = parser.parse_args()
+	extract(args.env_name, args.episodes, args.steps, args.box2d, args.size)
 
 
 
---
+++ vae-cnn/encode_images_func.py
@@ -0,0 +1,51 @@
+from vae import VAE
+import numpy as np
+import scipy.misc
+import matplotlib.pyplot as plt
+import argparse
+
+def encode(_input, weights, latent, reconstruct):
+    conv_vae = VAE()
+    conv_vae.make_vae(_input + ".npz", int(latent))
+    conv_vae.load_model(weights)
+
+    latent_vectors = []
+    # for i in range(len(conv_vae.x_test)):
+    #     latent_vectors.append(conv_vae.encode_image(conv_vae.x_test[i:i+1]))
+    raw_data = np.load(_input + ".npz")
+    for f in sorted(raw_data.files):
+        images = raw_data[f]
+        latent_vectors.append([conv_vae.encode_image(np.array([image]))[0] for image in images])
+
+    np.savez_compressed(_input + "_latent.npz", *latent_vectors)
+    if reconstruct:
+        data = np.load(_input + "_latent.npz")
+        # files = data.files
+        # vectors = np.array(data[files[0]])
+        reconstructed_images = []
+        for f in sorted(data.files):
+            latents = data[f]
+            # a = a.reshape(-1, a.shape[-2], a.shape[-1])
+            reconstructed_images.append([conv_vae.decode_latent(np.array([l]))[0] for l in latents])
+        # print(np.shape(np.array(reconstructed_images)))
+
+        # plt.imshow(reconstructed_images[1])
+
+        # plt.show()
+        np.savez_compressed(_input + "_recon.npz", *reconstructed_images)
+
+if __name__ == '__main__':
+    # make this in to argument parser
+    parser = argparse.ArgumentParser()
+    help_ = "Name of input without npzs"
+    parser.add_argument("-i", "--input", help=help_)
+    help_ = "Name of weights file"
+    parser.add_argument("-w", "--weights", help=help_)
+    help_ = "Dim of latent vec"
+    parser.add_argument("-l", "--latent", help=help_)
+    help_ = "Output reconstructed images as well"
+    parser.add_argument("-r", "--reconstruct", help=help_, action='store_true')
+
+    args = parser.parse_args()
+    encode(args.input, args.weights, args.latent, args.reconstruct)
+
