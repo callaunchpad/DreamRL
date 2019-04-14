3f406f0a29136be4bc4006fe12fcc1462d06f72f
Andrew Chen <3952722+andrewchenk@users.noreply.github.com>
2019-04-13 17:07:05 -0700

    Made encode_images.py to Jonathan's liking

--- vae-cnn/encode_images.py
+++ vae-cnn/encode_images.py
@@ -8,46 +8,42 @@ if __name__ == '__main__':
     conv_vae = VAE()
     # make this in to argument parser
     parser = argparse.ArgumentParser()
-    help_ = "Name of input without npzs"
+    help_ = "Name of input file without file extension"
     parser.add_argument("-i", "--input", help=help_)
-    help_ = "Name of weights file"
+    help_ = "Name of weights file without file extension"
     parser.add_argument("-w", "--weights", help=help_)
-    help_ = "Dim of latent vec"
-    parser.add_argument("-l", "--latent", help=help_)
-    help_ = "Output reconstructed images as well"
+    help_ = "Name of latents file"
+    parser.add_argument("-l", "--latents", help=help_)
+    help_ = "Dimension of latent vectors"
+    parser.add_argument("-d", "--dimension", help=help_)
+    help_ = "Output encoded latent vectors in _latent.npz file"
+    parser.add_argument("-e", "--encode", help=help_, action='store_true')
+    help_ = "Output reconstructed images in _recon.npz file (if only this flag is on, then assume _latent.npz inputs)"
     parser.add_argument("-r", "--reconstruct", help=help_, action='store_true')
 
     args = parser.parse_args()
-    conv_vae.make_vae(args.input + ".npz", int(args.latent))
-    conv_vae.load_model(args.weights)
 
-    # columns = 2
-    # rows = 3
-    # fig=plt.figure(figsize=(8, 8))
-    # t=1
-    # for i in range(1, columns*rows +1):
-    #     if (i % 2 == 1):
-    #         img = conv_vae.x_test[t-1:t]*255
-    #         t += 1
-    #     else:
-    #         img = conv_vae.decode_latent(conv_vae.encode_image(img))
-    #     fig.add_subplot(rows, columns, i)
-    #     plt.imshow(img[0])
-    # plt.show()
+    conv_vae.make_vae(args.input + ".npz", int(args.dimension))
+    conv_vae.load_model(args.weights + ".h5")
 
     latent_vectors = []
-    # for i in range(len(conv_vae.x_test)):
-    #     latent_vectors.append(conv_vae.encode_image(conv_vae.x_test[i:i+1]))
     raw_data = np.load(args.input + ".npz")
-    for f in sorted(raw_data.files):
-        images = raw_data[f]
-        latent_vectors.append([conv_vae.encode_image(np.array([image]))[0] for image in images])
+    if args.encode: 
+        for f in sorted(raw_data.files):
+            images = raw_data[f]
+            latent_vectors.append([conv_vae.encode_image(np.array([image]))[0] for image in images])
+        np.savez_compressed(args.input + "_latent.npz", *latent_vectors)
+    
 
-    np.savez_compressed(args.input + "_latent.npz", *latent_vectors)
     if args.reconstruct:
-        data = np.load(args.input + "_latent.npz")
+        data = ""  
+        if args.latents: 
+            data = np.load(args.latents + ".npz") 
+        else: 
+            data = np.load(args.input + "_latent.npz")
         # files = data.files
         # vectors = np.array(data[files[0]])
+    
         reconstructed_images = []
         for f in sorted(data.files):
             latents = data[f]
