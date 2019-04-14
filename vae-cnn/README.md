47c8c0c0f49c412fc5cca30e4e171fc853702d3a
Andrew Chen <3952722+andrewchenk@users.noreply.github.com>
2019-04-13 17:41:48 -0700

    Rewrote process_images to function form

---
+++ vae-cnn/README.md
@@ -0,0 +1,3 @@
+How to use 
+python vae_process_images.py LunarLander-v2_img_10_200 LunarLander_64 64 -e -r 
+python vae_process_images.py LunarLander-v2_img_10_200 LunarLander_64 64 -l LunarLander-v2_img_10_200_latent -r
---
+++ vae-cnn/vae_process_images.py
@@ -0,0 +1,63 @@
+from vae import VAE
+import numpy as np
+import scipy.misc
+import matplotlib.pyplot as plt
+import argparse
+
+def vae_process_images(images, weights, dimension, latents, encode, reconstruct):
+    conv_vae = VAE()
+    conv_vae.make_vae(images + ".npz", int(dimension))
+    conv_vae.load_model(weights + ".h5")
+
+    latent_vectors = []
+    raw_data = np.load(images + ".npz")
+    if encode: 
+        for f in sorted(raw_data.files):
+            file_images = raw_data[f]
+            latent_vectors.append([conv_vae.encode_image(np.array([image]))[0] for image in file_images])
+        np.savez_compressed(images + "_latent.npz", *latent_vectors)
+    
+    if reconstruct:
+        data = ""  
+        if latents: 
+            data = np.load(latents + ".npz") 
+        else: 
+            data = np.load(images + "_latent.npz")
+        # files = data.files
+        # vectors = np.array(data[files[0]])
+    
+        reconstructed_images = []
+        for f in sorted(data.files):
+            latents = data[f]
+            # a = a.reshape(-1, a.shape[-2], a.shape[-1])
+            reconstructed_images.append([conv_vae.decode_latent(np.array([l]))[0] for l in latents])
+        # print(np.shape(np.array(reconstructed_images)))
+
+        # plt.imshow(reconstructed_images[1])
+
+        # plt.show()
+        np.savez_compressed(images + "_recon.npz", *reconstructed_images)
+
+
+
+if __name__ == '__main__':
+    # make this in to argument parser
+    parser = argparse.ArgumentParser()
+    help_ = "Name of images file without file extension"
+    parser.add_argument("images", help=help_)
+    help_ = "Name of weights file without file extension"
+    parser.add_argument("weights", help=help_)
+    help_ = "Name of latents file"
+    parser.add_argument("dimension", help=help_)
+    help_ = "Output encoded latent vectors in _latent.npz file"
+    parser.add_argument("-l", "--latents", help=help_)
+    help_ = "Dimension of latent vectors"
+    parser.add_argument("-e", "--encode", help=help_, action='store_true')
+    help_ = "Output reconstructed images in _recon.npz file (if only this flag is on, then assume _latent.npz inputs)"
+    parser.add_argument("-r", "--reconstruct", help=help_, action='store_true')
+    args = parser.parse_args()
+
+    vae_process_images(args.images, args.weights, args.dimension, args.latents, args.encode, args.reconstruct)
+
+
+   
