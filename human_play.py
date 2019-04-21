dd24e400936b5af693fd341887d3ce208cb5b3cc
Sumer Kohli <sumer.kohli@berkeley.edu>
2019-04-20 17:32:48 -0700

    Rewrite play script to allow plotting rewards after game is over & game timeout

--- human_play.py
+++ human_play.py
@@ -1,23 +1,55 @@
 import gym
 from gym.utils import play
 import argparse
+import pygame
+import matplotlib
+from threading import Timer
+try:
+    matplotlib.use('TkAgg')
+    import matplotlib.pyplot as plt
+except ImportError as e:
+    logger.warn('failed to set matplotlib backend, plotting will not work: %s' % str(e))
+    plt = None
+from collections import deque
 
 parser = argparse.ArgumentParser(description='Have a human play a specific gym.')
 parser.add_argument('env_name', type=str, help='Environment name (e.g., Pong-v4)')
 parser.add_argument('--plot-rewards', default=False, help='Plot rewards while playing (warning: can cause keyboard input glitches)', action='store_true')
-parser.add_argument('--zoom-level', default=3, type=float, help='Set custom zoom level')
+parser.add_argument('--zoom-level', default=3.0, type=float, help='Set custom zoom level (default is 3.0)')
+parser.add_argument('--timeout', default=10.0, type=float, help='Game timeout (default is 10.0 seconds)')
 
 def main():
+	# Define reward callback
+	timestep = 0
 	def callback(obs_t, obs_tp1, action, rew, done, info):
-	    return [rew,]
+		nonlocal timestep
+		timestep += 1
+		data.append(rew)
 
-	plotter = play.PlayPlot(callback, 30 * 5, ["reward"])
-	env = gym.make(args.env_name)
+	# Initialize data structures for plotting
+	fig, ax = plt.subplots(1)
+	ax.set_title("Reward over time")
+	horizon_timesteps = int(30 * args.timeout)
+	data = deque(maxlen = horizon_timesteps)
 	if not args.plot_rewards:
 		callback = None
-	else:
-		callback = plotter.callback
-	play.play(env, zoom=args.zoom_level, callback=callback)
+
+	# Initialize game timer
+	t = Timer(args.timeout, lambda: pygame.quit())
+	t.start()
+
+	# Run main game loop
+	try:
+		env = gym.make(args.env_name)
+		play.play(env, fps=30, zoom=args.zoom_level, callback=callback)
+	except pygame.error:
+		pass
+
+	# Plot rewards over time before quitting
+	xmin, xmax = max(0, timestep - horizon_timesteps), timestep
+	ax.scatter(range(xmin, xmax), list(data), c='blue')
+	ax.set_xlim(xmin, xmax)
+	plt.show()
 
 if __name__ == '__main__':
 	args = parser.parse_args()
